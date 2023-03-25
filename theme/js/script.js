4adca4dad2058e595e91a40826ade11b3f13da0d
Tseing <im.yczeng@foxmail.com>
2023-03-25 20:02:20 +0800

    add map loading animation

--- theme/js/script.js
+++ theme/js/script.js
@@ -206,4 +206,18 @@ jQuery(function($) {
 			$(this).parent().addClass('activated');
 		});
 	}
+
+	/* ==========================================================================
+	   My Footprint Map Loading
+	   ========================================================================== */
+
+	let loader =`<div class="loader">
+      <svg class="circular" viewBox="25 25 50 50">
+        <circle class="path" cx="50" cy="50" r="20" fill="none" stroke-width="2" stroke-miterlimit="10"/>
+      </svg>
+  	</div>`;
+	$("#map").prepend(loader);
+	document.getElementById("map-html").onload = function() {
+		$(".loader").remove();
+	};
 });
