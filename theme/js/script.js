e61ca26c1777908f82a574d204c49697a7343733
Tseing <im.yczeng@foxmail.com>
2024-04-12 21:11:28 +0800

    2024.04.12 update theme attila v2.0

--- theme/js/script.js
+++ theme/js/script.js
@@ -1,251 +1,194 @@
 jQuery(function($) {
 
-	var body = $('body');
-	var html = $('html');
-	var viewport = $(window);
+  var html = $('html');
+  var viewport = $(window);
 
-	/* ==========================================================================
-	   Menu
-	   ========================================================================== */
+/* ==========================================================================
+   Menu
+   ========================================================================== */
 
-	function menu() {
-		html.toggleClass('menu-active');
-	};
+  function menu() {
+    html.toggleClass('menu-active');
+  };
 
-	$('#menu').on({
-		'click': function() {
-			menu();
-		}
-	});
+  $('#menu').on({
+    'click': function() {
+      menu();
+    }
+  });
 
-	$('.menu-button').on({
-		'click': function() {
-			menu();
-		}
-	});
+  $('.nav-menu').on({
+    'click': function() {
+      menu();
+    }
+  });
 
-	$('.hidden-close').on({
-		'click': function() {
-			menu();
-		}
-	});
+  $('.nav-close').on({
+    'click': function() {
+      menu();
+    }
+  });
 
-	/* ==========================================================================
-	   Search (TODO)
-	   ========================================================================== */
-	// function search() {
-	// 	html.toggleClass('search-active');
-	// };
+  viewport.on({
+    'resize': function() {
+      html.removeClass('menu-active');
+    },
+    'orientationchange': function() {
+      html.removeClass('menu-active');
+    }
+  });
 
-    // function showSearch() {
-	// 	search();
-	// 	$('html').css('overflow-y', 'hidden');
-    //     $('#search-wrapper').css('display','block');
-	// 	$('#search-wrapper').css('opacity','1');
-    // };
+/* ==========================================================================
+   Parallax cover
+   ========================================================================== */
 
-    // function closeSearch() {
-	// 	search();
-	// 	$('html').css('overflow-y', 'unset');
-    //     $('#search-wrapper').css('display','none');
-	// 	$('#search-wrapper').css('opacity','0');
-	// 	$(".input-field").val("");
-    // };
+  var cover = $('.cover');
+  var coverPosition = 0;
 
-	// // function click_body_to_close_search() {
-	// // 	var search_active = document.getElementsByClassName('search-active');
-	// // 	search_active ? closeSearch(): {};
-	// // };
+  function prlx() {
+    if (cover.length >= 1) {
+      var windowPosition = viewport.scrollTop();
+      (windowPosition > 0) ? coverPosition = Math.floor(windowPosition * 0.25): coverPosition = 0;
+      cover.css({
+        '-webkit-transform': 'translate3d(0, ' + coverPosition + 'px, 0)',
+        'transform': 'translate3d(0, ' + coverPosition + 'px, 0)'
+      });
+      if (viewport.scrollTop() < cover.height()) {
+        html.addClass('cover-active');
+        html.removeClass('cover-inactive');
+      }
+      else {
+        html.removeClass('cover-active');
+        html.addClass('cover-inactive');
+      }
+    }
+  }
+  prlx();
 
-	// $('.search-button').on({
-	// 	'click': function() {
-	// 		showSearch();
-	// 	}
-	// });
+  viewport.on({
+    'scroll': function() {
+      prlx();
+    },
+    'resize': function() {
+      prlx();
+    },
+    'orientationchange': function() {
+      prlx();
+    }
+  });
 
-	// $('.search-close').on({
-	// 	'click':function() {
-	// 		closeSearch();
-	// 	}
+/* ==========================================================================
+   Gallery
+   ========================================================================== */
 
-	// });
+  function gallery() {
+    'use strict';
+    var images = document.querySelectorAll('.kg-gallery-image img');
+    images.forEach(function(image) {
+      var container = image.closest('.kg-gallery-image');
+      var width = image.attributes.width.value;
+      var height = image.attributes.height.value;
+      var ratio = width / height;
+      container.style.flex = ratio + ' 1 0%';
+    });
+  }
+  gallery();
 
-	// document.body.onclick = function() {
-	// 	click_body_to_close_search();
-	// };
+/* ==========================================================================
+  My Footprint Map Loading
+  ========================================================================== */
 
-	// $('.search').on({
-	// 	'click':function() {
-	// 		event.stopPropagation();
-	// 	}
-	// });
-
-	/* ==========================================================================
-	   Parallax cover
-	   ========================================================================== */
-
-	var cover = $('.cover');
-	var coverPosition = 0;
-
-	function prlx() {
-		if(cover.length >= 1) {
-			var windowPosition = viewport.scrollTop();
-			(windowPosition > 0) ? coverPosition = Math.floor(windowPosition * 0.25) : coverPosition = 0;
-			cover.css({
-				'-webkit-transform' : 'translate3d(0, ' + coverPosition + 'px, 0)',
-				'transform' : 'translate3d(0, ' + coverPosition + 'px, 0)'
-			});
-			if(viewport.scrollTop() < cover.height()){
-				html.addClass('cover-active');
-				html.removeClass('cover-inactive');
-			}
-			else{
-				html.removeClass('cover-active');
-				html.addClass('cover-inactive');
-			}
-			// (viewport.scrollTop() < cover.height()) ? html.addClass('cover-active') : html.removeClass('cover-active');
-			// (viewport.scrollTop() > cover.height()) ? html.addClass('cover-inactive') : html.removeClass('cover-inactive');
-		}
-	}
-	prlx();
-
-	viewport.on({
-		'scroll': function() {
-			prlx();
-		},
-		'resize': function() {
-			prlx();
-		},
-		'orientationchange': function() {
-			prlx();
-		}
-	});
-
-	/* ==========================================================================
-	   Reading Progress
-	   ========================================================================== */
-
-	var post = $('.post-content');
-
-	function readingProgress() {
-		if(post.length >= 1) {
-			var postBottom = post.offset().top + post.height();
-			var windowBottom = viewport.scrollTop() + viewport.height();
-			var progress = 100 - (((postBottom - windowBottom) / (postBottom - viewport.height())) * 100);
-			$('.progress-bar').css('width', progress + '%');
-			(progress > 100) ? $('.progress-container').addClass('ready') : $('.progress-container').removeClass('ready');
-		}
-	}
-	readingProgress();
-
-	viewport.on({
-		'scroll': function() {
-			readingProgress();
-		},
-		'resize': function() {
-			readingProgress();
-		},
-		'orientationchange': function() {
-			readingProgress();
-		}
-	});
-
-	/* ==========================================================================
-	   Gallery
-	   ========================================================================== */
-
-	function gallery() {
-		var images = document.querySelectorAll('.kg-gallery-image img');
-		images.forEach(function (image) {
-			var container = image.closest('.kg-gallery-image');
-			var width = image.attributes.width.value;
-			var height = image.attributes.height.value;
-			var ratio = width / height;
-			container.style.flex = ratio + ' 1 0%';
-		});
-	}
-	gallery();
-
-	/* ==========================================================================
-	   Style code blocks with highlight and numbered lines
-	   ========================================================================== */
-
-	// function codestyling() {
-	// 	var mdSelector=".highlight pre code"
-	// 	var rstSelector=".highlight pre"
-	// 	var selector=rstSelector
-	// 	var showLines=false
-
-	// 	if($(mdSelector).length) {
-	// 		selector = mdSelector
-	// 		showLines = true
-	// 	}
-
-	// 	$(selector).each(function(i, e) {
-	// 		// Code highlight
-	// 		hljs.highlightBlock(e);
-
-	// 		// No lines for plain text blocks and nolines
-	// 		if(!($(this).hasClass('language-text') && $(this).hasClass('nolines')) && showLines) {
-	// 			var code = $(this);
-	// 			// Calculate amount of lines
-	// 			var lines = code.html().split(/\n(?!$)/g).length;
-	// 			var numbers = [];
-	// 			if (lines > 1) {
-	// 				lines++;
-	// 			}
-	// 			for (i = 1; i < lines; i++) {
-	// 				numbers += '<span class="line" aria-hidden="true">' + i + '</span>';
-	// 			}
-	// 			code.parent().append('<div class="lines">' + numbers + '</div>');
-	// 		}
-	// 	});
-	// }
-
-	// Format code blocks only
-	// function codestylingWithoutLineNumbers() {
-	// 	$(".highlight pre").each(function(i, e) {
-	// 		// Code highlight
-	// 		hljs.highlightBlock(e);
-	// 	});
-	// }
-
-	// codestylingWithoutLineNumbers();
-
-	/* ==========================================================================
-	   Initialize and load Disqus
-	   ========================================================================== */
-
-	if (typeof disqus === 'undefined') {
-		$('.post-comments').css({
-			'display' : 'none'
-		});
-	} else {
-		$('#show-disqus').on('click', function() {
-			$.ajax({
-				type: "GET",
-				url: "//" + disqus + ".disqus.com/embed.js",
-				dataType: "script",
-				cache: true
-			});
-			$(this).parent().addClass('activated');
-		});
-	}
-
-	/* ==========================================================================
-	   My Footprint Map Loading
-	   ========================================================================== */
-
-	let loader =`<div class="loader">
+  let loader =`<div class="loader">
       <svg class="circular" viewBox="25 25 50 50">
         <circle class="path" cx="50" cy="50" r="20" fill="none" stroke-width="2" stroke-miterlimit="10"/>
       </svg>
-  	</div>`;
+    </div>`;
 
-	if ($("#map").length){
-		$("#map").prepend(loader);
-		document.getElementById("map-html").onload = function() {
-			$(".loader").remove();
-		};
-	}
+  if ($("#map").length){
+    $("#map").prepend(loader);
+    document.getElementById("map-html").onload = function() {
+      $(".loader").remove();
+    };
+  }
+
+/* ==========================================================================
+   Theme
+   ========================================================================== */
+
+  function theme() {
+    'use strict';
+    var toggle = $('.js-theme');
+    var toggleText = toggle.find('.theme-text');
+
+    function system() {
+      html.removeClass(['theme-dark', 'theme-light']);
+      localStorage.removeItem('attila_theme');
+      toggleText.text(toggle.attr('data-system'));
+    }
+
+    function dark() {
+      html.removeClass('theme-light').addClass('theme-dark');
+      localStorage.setItem('attila_theme', 'dark');
+      toggleText.text(toggle.attr('data-dark'));
+    }
+
+    function light() {
+      html.removeClass('theme-dark').addClass('theme-light');
+      localStorage.setItem('attila_theme', 'light');
+      toggleText.text(toggle.attr('data-light'));
+    }
+
+    switch (localStorage.getItem('attila_theme')) {
+      case 'dark':
+        dark();
+      break;
+      case 'light':
+        light();
+      break;
+      default:
+        system();
+      break;
+    }
+
+    toggle.on('click', function (e) {
+      e.preventDefault();
+
+      if (!html.hasClass('theme-dark') && !html.hasClass('theme-light')) {
+        dark();
+      } else if (html.hasClass('theme-dark')) {
+        light();
+      } else {
+        system();
+      }
+    });
+  }
+  theme();
 });
+
+/* ==========================================================================
+Copy Info
+========================================================================== */
+function copyURL(event, url) {
+  navigator.clipboard.writeText(url);
+  popInfo(event, "URL Copied!");
+};
+
+function popInfo(event, info) {
+  var x = event.clientX;
+  var y = event.clientY;
+  popDiv = $("<div>", { class: 'pop-info', html: info });
+  popDiv.css({
+      'position':'fixed',
+      'left': x + 'px',
+      'top': y - 35 + 'px',
+  });
+  $('body').append(popDiv);
+  popDiv.animate(
+    {
+      top: y - 100 + 'px',
+      opacity: '0.1'
+    },
+    1200,
+    "swing",
+    function(){$(this).remove()});
+};
